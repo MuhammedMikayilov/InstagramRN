1b7a4f84ed53c1da4e2d497847ec26213e95b006
MikayilovMuhammed <92036426+MikayilovMuhammed@users.noreply.github.com>
2022-04-08 01:44:55 +0400

    Initial

---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Home screen</Text>
+    </SafeAreaView>
+  );
+}
+
+export default HomeScreen;
